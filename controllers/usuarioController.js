02365774bd95a46279dae1f77256318d89a65cfc
LeeDaeMin <laalmeida@uts.edu.co>
2023-02-13 23:33:17 -0500

    add controller CrearNuevoUsuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,16 @@
+const Usuarios = require('../models/Usuarios');
+
 exports.FormCrearCuenta =  (req, res)=>{
     res.render('crear-cuenta', {
         nombrePagina : 'Crea Tu Cuenta'
     })
 };
+
+exports.crearNuevoUsuario = async (req, res) =>{
+    const usuario = req.body;
+
+    const nuevoUsuario = await Usuarios.create(usuario);
+
+    console.log('Usuarios Creado', nuevoUsuario)
+
+}
